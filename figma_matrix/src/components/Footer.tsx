4da34b4aca8513dc194fd9b648904305f1dae86f
Peketi <pavani1642002@gmail.com>
2024-10-15 19:00:17 +0530

    final

--- figma_matrix/src/components/Footer.tsx
+++ figma_matrix/src/components/Footer.tsx
@@ -1,117 +1,63 @@
-import {
-    Box,
-    chakra,
-    Container,
-    SimpleGrid,
-    Stack,
-    VisuallyHidden,
-    useColorModeValue,
-  } from "@chakra-ui/react";
-  import { ReactNode } from "react";
-  import { FaInstagram, FaTelegramPlane, FaTwitter } from "react-icons/fa";
-  import { FaDiscord, FaFacebookF } from "react-icons/fa6";
-  
-  const SocialButton = ({
-    children,
-    label,
-    href,
-  }: {
-    children: ReactNode;
-    label: string;
-    href: string;
-  }) => {
-    return (
-      <chakra.button
-        bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-        rounded={"full"}
-        w={8}
-        h={8}
-        cursor={"pointer"}
-        as={"a"}
-        href={href}
-        display={"inline-flex"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        transition={"background 0.3s ease"}
-        _hover={{
-          bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-        }}
-      >
-        <VisuallyHidden>{label}</VisuallyHidden>
-        {children}
-      </chakra.button>
-    );
-  };
-  
-  export default function Footer() {
-    return (
-      <Box className="bg-[#010e14] mt-[70px]" color={"white"}>
-        <Container as={Stack} maxW={"6xl"} py={10}>
-          <SimpleGrid
-            templateColumns={{ base: "1fr", md: "1fr 1fr" }}
-            spacing={8}
-          >
-            <Stack spacing={6} align={{ base: "center", md: "flex-start" }}>
-              <Box>
-                <div className="flex items-center gap-[5px]">
-                  <div className="grid grid-cols-3 gap-[1px] p-2 max-w-max">
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg1 rounded-tr-[100px]"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg2"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg2 rounded-tr-[100px]"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg2"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-none"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg2"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg2 rounded-bl-[100px]"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg1 rounded-br-[100px]"></div>
-                    <div className="w-[11.31px] h-[11.31px] bg-logoBg2 rounded-bl-[100px]"></div>
-                  </div>
-                  <div className="text-[24px] font-bold leading-[22px] tracking-[-0.03px] text-white font-play">
-                    EthAi
-                  </div>
+"use client"
+
+import React from 'react'
+import { FaTelegramPlane,FaInstagram,FaFacebookF ,FaDiscord} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import AOS from 'aos';
+import { useEffect } from 'react';
+import 'aos/dist/aos.css';
+import "./styles/footer.css"
+const Footer = () => {
+    useEffect(() => {
+        AOS.init({ duration: 1200 });
+      }, []);
+  return (
+    <div className='footercontainer'>
+        <div className='insidefooter' data-aos="fade-up"> 
+            <div className='leftfooter'>
+                <div className='brandname'>
+                <div className="text-2xl flex font-bold text-gray-800 ">
+            <div>
+            <svg width="37" height="36" viewBox="0 0 37 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="12.3428" width="11.3143" height="11.3143" fill="#1ADEF5"/>
+<path d="M0.5 0C6.7487 0 11.8143 5.06558 11.8143 11.3143H0.5V0Z" fill="#1E808C"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M11.8143 7.05628V0C10.0746 0 8.4265 0.392677 6.95395 1.09424C9.15929 2.48982 10.8818 4.57953 11.8143 7.05628Z" fill="#1E808C"/>
+<rect x="12.8429" width="11.3143" height="11.3143" fill="#1ADEF5"/>
+<path d="M25.1857 0C31.4344 0 36.5 5.06558 36.5 11.3143H25.1857V0Z" fill="#1ADEF5"/>
+<rect x="25.1857" y="12.3428" width="11.3143" height="11.3143" fill="#1ADEF5"/>
+<path d="M25.1857 24.6858H36.5V36.0001C30.2513 36.0001 25.1857 30.9345 25.1857 24.6858Z" fill="#1ADEF5"/>
+<path d="M0.5 24.6858H11.8143V36.0001C5.56558 36.0001 0.5 30.9345 0.5 24.6858Z" fill="#1ADEF5"/>
+<path d="M12.8429 24.6858H24.1572C24.1572 30.9345 19.0916 36.0001 12.8429 36.0001V24.6858Z" fill="#1E808C"/>
+</svg>
+            </div>
+            <div style={{color:"white"}}>
+            EathAi
+            </div>
+          
+            </div>
                 </div>
-              </Box>
-  
-              <Stack direction={"row"} spacing={6}>
-                <SocialButton label={"Twitter"} href={"#"}>
-                  <FaTelegramPlane />
-                </SocialButton>
-                <SocialButton label={"YouTube"} href={"#"}>
-                  <FaInstagram />
-                </SocialButton>
-                <SocialButton label={"Instagram"} href={"#"}>
-                  <FaTwitter />
-                </SocialButton>
-                <SocialButton label={"YouTube"} href={"#"}>
-                  <FaFacebookF />
-                </SocialButton>
-                <SocialButton label={"Instagram"} href={"#"}>
-                  <FaDiscord />
-                </SocialButton>
-              </Stack>
-            </Stack>
-            <Stack
-              align={"flex-start"}
-              spacing={4}
-              display={{ base: "none", sm: "none", md: "flex" }}
-            >
-              <h1 className="text-[32px] lg:text-[32px] md:text-[24px]   font-bold text-white font-space">
-                “Designed for traders of today, just like you.”
-              </h1>
-              <div className="flex items-center">
-                <div className="relative w-[520px] md:w-[420px] h-[50px] max-w-[520px] border-[1px] border-white rounded-[12px] p-[2px]">
-                  <input
-                    type="email"
-                    placeholder="What’s your work email"
-                    className="w-full h-[44px] pl-4 pr-24 rounded-[12px] bg-footerBg text-white focus:bg-whiteAlpha-200 border-none"
-                  />
-                  <button className="absolute inset-y-0 right-0 md:right-0  h-[42px] px-6 text-white bg-dApp border border-dAppBorder rounded-[12px] hover:bg-navButtonColor hover:text-black hover:font-medium mt-1 mr-1 mb-1">
-                    Open dApp
-                  </button>
+            
+            <div className='icons'>
+            <FaTelegramPlane />
+            <FaInstagram />
+            <FaXTwitter />
+            <FaFacebookF />
+            <FaDiscord />
+            </div>
+            </div>
+            <div className='rightfooter'>
+                <div className='footerbootom'>
+                    <p className='textfooter'>Designed for traders of today, just like you.</p>
+                  
                 </div>
-              </div>
-            </Stack>
-          </SimpleGrid>
-        </Container>
-      </Box>
-    );
-  }
+                <div className='searchcontainer'>
+                    <input type="text" placeholder='write your working email' />
+                    <button className='searchbutton'>Get Started</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
